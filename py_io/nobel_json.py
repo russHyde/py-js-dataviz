3eef8ede793489b7ed75bd88d779e3c8a1d9ed92
Russ Hyde <russ.hyde.data@gmail.com>
2021-02-03 15:38:41 +0000

    ch3: write json using json.dump (and read with base python)

---
+++ py_io/nobel_json.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+from nobel import NOBEL_WINNERS
+
+
+def write_json(list_of_dicts, file_path):
+    """
+    Writes a list of dictionaries to a file
+    """
+    with open(file_path, "w") as file_writer:
+        json.dump(list_of_dicts, file_writer)
+
+
+def read_json(file_path):
+    """
+    Reads some data from a json file
+    """
+    with open(file_path, "r") as file_reader:
+        return file_reader.read()
+
+
+if __name__ == "__main__":
+    FILE = os.path.join("data", "nobel_winners.json")
+
+    write_json(NOBEL_WINNERS, FILE)
+
+    data = read_json(FILE)
+    print(data)
