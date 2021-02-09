06308a28924f0e57645907a09f637174119373ef
Russ Hyde <russ.hyde.data@gmail.com>
2021-02-09 16:23:14 +0000

    ch3: output .csv from {dataset} no longer works

--- py_io/nobel_using_dataset_package.py
+++ py_io/nobel_using_dataset_package.py
@@ -23,6 +23,7 @@ def create_winners_table(db):
 if __name__ == "__main__":
     URL = "sqlite:///data/nobel_prize.db"
     DB = dataset.connect(URL)
+    # CSV = "data/nobel_winners_ds.csv"
 
     print(winners(DB))
 
@@ -35,3 +36,10 @@ if __name__ == "__main__":
     # Remake the winners table using `dataset` syntax
     create_winners_table(DB)
     print(winners(DB))
+
+    # Make a csv from the winners table using {dataset}
+    #   - this no longer works, .freeze has been moved to {datafreeze} which is
+    #   not under active development (hasn't been released in 4 years)
+    #   - ? recommend using pandas.to_csv and pandas.read_sql_query
+    # winner_set = DB["winners"].find()
+    # dataset.freeze(winner_set, format="csv", filename=CSV)
